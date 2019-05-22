90df697c620ccec811216bbc412efd8e8ee37ddb
John Moen <entity.john@gmail.com>
2019-05-21 19:57:00 -0500

    create nav skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Nav, NavLink } from 'reactstrap'
+
+import Store from './Store.js'
+import Cart from './Cart.js'
+import Inventory from './Inventory.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div className="slider-menu">
+        <Nav vertical>
+          <NavLink href="/store">Store</NavLink>
+          <NavLink href="/cart">Cart</NavLink>
+          <NavLink href="/inventory">Inventory</NavLink>
+        </Nav>
+      </div>
+      <div className="container page-wrapper">
+        <Router>
+          <Route path='/store' component={Store} />
+          <Route path='/cart' component={Cart} />
+          <Route path='/inventory' component={Inventory} />
+        </Router>
+      </div>
     </div>
   );
 }
