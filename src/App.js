8c0b48c31596cc5dd06a4683b904305bc8e42ffe
John Moen <entity.john@gmail.com>
2019-05-22 11:37:05 -0500

    working on adding context hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Nav, NavLink } from 'reactstrap'
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import { AppContextProvider } from './context'
 
 import Store from './Store.js'
 import Cart from './Cart.js'
 import Inventory from './Inventory.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style/App.scss';
+import Header from './components//Header.js'
+
+import fishes from './sample-fishes'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/App.scss'
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <div className="slider-menu">
-        <Nav vertical>
-          <NavLink href="/store">Store</NavLink>
-          <NavLink href="/cart">Cart</NavLink>
-          <NavLink href="/inventory">Inventory</NavLink>
-        </Nav>
-      </div>
-      <div className="container page-wrapper">
-        <Router>
-          <Route path='/store' component={Store} />
-          <Route path='/cart' component={Cart} />
-          <Route path='/inventory' component={Inventory} />
-        </Router>
-      </div>
-    </div>
-  );
+	return (
+		<AppContextProvider fishes={fishes} sidenavInitial={false}>
+			<div className="app-wrapper">
+				<Header />
+				<div className="container page-wrapper">
+					<Router>
+						<Route path="/store" component={Store} />
+						<Route path="/cart" component={Cart} />
+						<Route path="/inventory" component={Inventory} />
+					</Router>
+				</div>
+			</div>
+		</AppContextProvider>
+	)
 }
 
-export default App;
+export default App
