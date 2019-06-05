e5bfc7ff26a07581ae7dd7221656fcf362c3d070
John Moen <entity.john@gmail.com>
2019-06-05 11:15:12 -0500

    add Cart page, build out Stripe based Checkout Form

--- src/Cart.js
+++ src/Cart.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
+import { Elements, StripeProvider } from 'react-stripe-elements'
+import CheckoutForm from './components/CheckoutForm'
+import { formatPrice, calculateTotal } from './helpers'
 
-export default () => {
-    return (
-        <div>
-            Cart!
-    </div>
-    )
+export default props => {
+	const { cart, resetOrder } = props
+	return (
+		<div>
+			<StripeProvider apiKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}>
+				<Elements>
+					<CheckoutForm totalPrice={calculateTotal(cart)} resetOrder={resetOrder} />
+				</Elements>
+			</StripeProvider>
+			<ListGroup>
+				{Object.values(cart).map(item => (
+					<ListGroupItem className="d-flex justify-content" key={`${item.id}`}>
+						<span>{item.name}</span>
+						<span>{`x ${item.quantity}`}</span>
+						<span>{formatPrice(item.price)}</span>
+					</ListGroupItem>
+				))}
+			</ListGroup>
+		</div>
+	)
 }
---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap'
+import { CardElement, injectStripe } from 'react-stripe-elements'
+import { formatPrice } from '../helpers'
+class CheckoutForm extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			complete: false,
+			completedCharge: {},
+			error: {},
+		}
+		this.createCharge = this.createCharge.bind(this)
+		this.useTestVisa = this.useTestVisa.bind(this)
+	}
+	async useTestVisa() {
+		let response = await fetch(`${process.env.REACT_APP_USETESTVISA_ENDPOINT}`, {
+			method: 'POST',
+			body: JSON.stringify({
+				charge: {
+					amount: this.props.totalPrice,
+					currency: 'usd',
+				},
+			}),
+		})
+		const data = await response.json()
+		console.log(data)
+		if (response.ok) {
+			this.setState({ complete: true, completedCharge: data })
+			this.props.resetOrder()
+		}
+	}
+	async createCharge() {
+		let { token } = await this.props.stripe.createToken({ name: 'Name' })
+		console.log(token)
+		let response = await fetch(`${process.env.REACT_APP_CREATE_CHARGE_ENDPOINT}`, {
+			method: 'POST',
+			body: JSON.stringify({
+				charge: {
+					amount: this.props.totalPrice,
+					currency: 'usd',
+				},
+				token: token.id,
+			}),
+		})
+		if (response.ok) {
+			const data = await response.json()
+			this.setState({ complete: true, completedCharge: data.charge })
+			this.props.resetOrder()
+		} else {
+			const error = await response.json()
+			this.setState({ error: error })
+		}
+	}
+
+	render() {
+		if (this.state.complete) return <h1>Purchase Complete</h1>
+		return (
+			<div className="checkout">
+				<p>Total Cost: {formatPrice(this.props.totalPrice)}</p>
+				<div className="jumbotron p-4">
+					<div className="m-3">
+						<CardElement />
+					</div>
+					<Button color="primary" block onClick={this.createCharge}>
+						Pay
+					</Button>
+				</div>
+				<div className="text-center m-3">OR</div>
+				<Button color="success" block onClick={this.useTestVisa}>
+					Use a test card:
+					<div className="small-print-card-info">Visa 4242 4242 4242 4242</div>
+				</Button>
+			</div>
+		)
+	}
+}
+
+export default injectStripe(CheckoutForm)
