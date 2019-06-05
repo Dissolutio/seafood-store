3300eefa4e5ebe5791de0f27ea4bba4cbe7d4f20
John Moen <entity.john@gmail.com>
2019-06-05 11:12:34 -0500

    wrap app in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+const EntryPoint = () => (
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+)
+ReactDOM.render(<EntryPoint />, document.getElementById('root'))
+serviceWorker.unregister()
