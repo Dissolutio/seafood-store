33ad1ab578677b96141361543f98285e1f0ca9b2
John Moen <entity.john@gmail.com>
2019-06-05 11:16:16 -0500

    refactor Header/Nav
    Bootstrap NavLinks do not work out of the box as react router replacements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,39 @@
-import React, { useContext } from 'react'
-import { Nav, NavLink } from 'reactstrap'
-import { AppContext } from '../context'
-export default () => {
-	const appContext = useContext(AppContext)
-	const { sidenavIsOpen, toggleSidenav } = appContext
+import React from 'react'
+import { Nav, NavItem, Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
+export default props => {
+	const { resetAppClick, resetAppDoublePress } = props
+	const buttonText = () => {
+		if (resetAppDoublePress === 1) {
+			return {
+				text: 'Really Reset App?',
+				color: 'danger',
+			}
+		} else {
+			return {
+				text: 'Reset App',
+				color: 'info',
+			}
+		}
+	}
 	return (
-		<>
-			<div className="page-header jumbotron">
-				<h1>Seafood Store</h1>
-				<button
-					className={`hamburger hamburger--slider ${sidenavIsOpen ? 'is-active' : ''}`}
-					type="button"
-					onClick={toggleSidenav}>
-					<span className="hamburger-box">
-						<span className="hamburger-inner" />
-					</span>
-				</button>
-			</div>
-			<div className={`sidenav ${sidenavIsOpen ? 'sidenav-open' : ''}`}>
-				<Nav card vertical>
-					<NavLink href="/">Store</NavLink>
-					<NavLink href="/cart">Cart</NavLink>
-					<NavLink href="/inventory">Inventory</NavLink>
-				</Nav>
-			</div>
-		</>
+		<header id="header" className="container mt-3">
+			<Nav tag="nav" className="d-flex justify-content-end">
+				<NavItem className="pl-3 pr-3">
+					<Link className="heading-2" to="/">
+						Seafood Store
+					</Link>
+				</NavItem>
+				<NavItem className="pl-3 pr-3">
+					<Link to="/cart">Cart</Link>
+				</NavItem>
+				<NavItem className="pl-3 pr-3">
+					<Link to="/inventory">Inventory</Link>
+				</NavItem>
+			</Nav>
+			<Button outline className="m-4" color={buttonText().color} onClick={resetAppClick}>
+				{buttonText().text}
+			</Button>
+		</header>
 	)
 }
