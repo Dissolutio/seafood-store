6da462b206b53cca9f3b349de92a572cb32acdbf
John Moen <entity.john@gmail.com>
2019-06-10 17:33:06 -0500

    refactor and tidy CheckoutForm stripe charge code

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import { Button } from 'reactstrap'
 import { CardElement, injectStripe } from 'react-stripe-elements'
-import { formatPrice } from '../helpers'
 class CheckoutForm extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			complete: false,
-			completedCharge: {},
 			error: {},
 		}
 		this.createCharge = this.createCharge.bind(this)
 		this.useTestVisa = this.useTestVisa.bind(this)
+	}
+	async completeCharge(response) {
+		const data = await response.json()
+		if (response.ok) {
+			console.log(data)
+			this.props.setOrderComplete(data)
+			this.props.emptyCart()
+		} else {
+			const error = await response.json()
+			this.setState({ error: error })
+		}
 	}
 	async useTestVisa() {
 		let response = await fetch(`${process.env.REACT_APP_USETESTVISA_ENDPOINT}`, {
@@ -23,11 +31,7 @@ class CheckoutForm extends Component {
 				},
 			}),
 		})
-		const data = await response.json()
-		if (response.ok) {
-			this.setState({ complete: true, completedCharge: data })
-			this.props.emptyCart()
-		}
+		this.completeCharge(response)
 	}
 	async createCharge() {
 		let { token } = await this.props.stripe.createToken({ name: 'Name' })
@@ -42,35 +46,30 @@ class CheckoutForm extends Component {
 				token: token.id,
 			}),
 		})
-		if (response.ok) {
-			const data = await response.json()
-			this.setState({ complete: true, completedCharge: data.charge })
-			this.props.emptyCart()
-		} else {
-			const error = await response.json()
-			this.setState({ error: error })
-		}
+		this.completeCharge(response)
 	}
 
 	render() {
-		if (this.state.complete) return <h1>Purchase Complete</h1>
 		return (
-			<div className="checkout">
-				<p>Total Cost: {formatPrice(this.props.totalPrice)}</p>
+			<>
 				<div className="jumbotron p-4">
-					<div className="m-3">
-						<CardElement />
-					</div>
-					<Button color="primary" block onClick={this.createCharge}>
-						Pay
+					<Button color="success" block onClick={this.useTestVisa}>
+						Pay with:
+						<div className="small-print-card-info">Visa ...4242</div>
 					</Button>
 				</div>
-				<div className="text-center m-3">OR</div>
-				<Button color="success" block onClick={this.useTestVisa}>
-					Use a test card:
-					<div className="small-print-card-info">Visa 4242 4242 4242 4242</div>
-				</Button>
-			</div>
+				<div className="jumbotron p-4">
+					<div className="text-center m-3">
+						Or enter card info (<a href="https://stripe.com/docs/testing#cards">use a stripe test card</a>)
+					</div>
+					<div className="p-5">
+						<CardElement />
+					</div>
+					<Button color="secondary" block onClick={this.createCharge}>
+						Submit Card
+					</Button>
+				</div>
+			</>
 		)
 	}
 }
